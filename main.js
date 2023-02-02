6a5629eeb5074ee4a877640b6873f21a28b61bac
Raghad Almughidi <52867161+RaghadAlmughidi@users.noreply.github.com>
2023-02-02 20:53:29 +0300

    create back btn and populaer page only header

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 let openMenu = document.querySelector('.open');
 let closeMenu = document.querySelector('.close');
 let ul = document.querySelector('ul');
+let backIcon=document.querySelector('.back__icon')
+
 openMenu.onclick = () => {
     ul.classList.add('active-nav');
-
 
 
 }
 closeMenu.onclick = () => {
     ul.classList.remove('active-nav');
-
-
-
 }
+
