ea29a5c83ba6f63427f2455e1c0f5b14a6e49b3d
Raghad Almughidi <52867161+RaghadAlmughidi@users.noreply.github.com>
2023-02-04 13:26:56 +0300

    fix gallary page and create view btn

---
+++ header.js
@@ -0,0 +1,13 @@
+
+let openMenu = document.querySelector('.open');
+let closeMenu = document.querySelector('.close');
+let ul = document.querySelector('ul');
+let backIcon=document.querySelector('.back__icon')
+openMenu.addEventListener ('click',()=>{ 
+
+    ul.classList.add('active-nav');
+})
+closeMenu.addEventListener ('click',()=>{ 
+
+    ul.classList.remove('active-nav');
+})
--- main.js
+++ main.js
@@ -1,14 +1,28 @@
-let openMenu = document.querySelector('.open');
-let closeMenu = document.querySelector('.close');
-let ul = document.querySelector('ul');
-let backIcon=document.querySelector('.back__icon')
-
-openMenu.onclick = () => {
-    ul.classList.add('active-nav');
 
 
-}
-closeMenu.onclick = () => {
-    ul.classList.remove('active-nav');
-}
+let fillTow=document.querySelector('.fill-two');
+let fillThree=document.querySelector('.full-lay')
+let fullLay=document.querySelector('.full-lay')
+let galleryCards = document.querySelector('.gallery-cards')
+
+
+
+
+  
+
+fillTow.addEventListener('click',()=>{
+  
+    galleryCards.classList.remove('gallery-cards');
+    galleryCards.classList.add('gallery-cards-2');
+   
+
+})
+
+fillThree.addEventListener('click',()=>{
+    galleryCards.classList.remove('gallery-cards-2');
+   
+    galleryCards.classList.add('gallery-cards');
+   
+
+})
 
