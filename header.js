2a1c7fe55e6239e09033e9aabb15959d157ae1dd
Raghad Almughidi <52867161+RaghadAlmughidi@users.noreply.github.com>
2023-02-17 15:15:12 +0300

    nav barat home psge  not working stil working on it

--- header.js
+++ header.js
@@ -2,12 +2,15 @@
 let openMenu = document.querySelector('.open');
 let closeMenu = document.querySelector('.close');
 let ul = document.querySelector('ul');
+let itemLink = document.querySelector('.item-link');
 let backIcon=document.querySelector('.back__icon');
 openMenu.addEventListener ('click',()=>{ 
 
-    ul.classList.add('active-nav');
+    ul.classList.add('active');
 })
 closeMenu.addEventListener ('click',()=>{ 
 
-    ul.classList.remove('active-nav');
+    ul.classList.remove('active');
+    
+
 })
